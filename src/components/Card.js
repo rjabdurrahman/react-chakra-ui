48f957df99a9179d65013e5f338d7a2238369b4c
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-06-04 11:59:43 +0600

    Renamed: toUSD function to currencyConverter

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,10 @@ import { Stack, Input, Box } from "@chakra-ui/react";
 import { useState } from 'react';
 
 function Card(props) {
-    let [amount, setAmount] = useState(toDoller(5000));
+    let [amount, setAmount] = useState(currencyConverter(5000));
     return (
         <Stack spacing={3}>
-            <Input onChange={(event) => setAmount(toDoller(event.target.value))} placeholder="5000" />
+            <Input onChange={(event) => setAmount(currencyConverter(event.target.value))} placeholder="5000" />
             <Box bg="white" h="250px" w="400px" p={6} borderWidth="1px" borderRadius="lg">
                 {amount}
             </Box>
@@ -13,7 +13,7 @@ function Card(props) {
     );
 }
 
-function toDoller(val) {
+function currencyConverter(val) {
     return (Number(val)).toLocaleString('en-US', {
         style: 'currency',
         currency: 'USD',

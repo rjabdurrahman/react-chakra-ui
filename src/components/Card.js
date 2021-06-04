cc4f47bdb8ae851f11c8873210ee2e234eecb356
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-06-04 12:09:52 +0600

    Added: Input group

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
-import { Stack, Input, Box, Text } from "@chakra-ui/react";
+import { Stack, InputGroup, Input, InputLeftAddon, Box, Text } from "@chakra-ui/react";
 import { useState } from 'react';
 
 function Card(props) {
     let [amount, setAmount] = useState(currencyConverter(5000));
     return (
         <Stack spacing={3}>
-            <Input onChange={(event) => setAmount(currencyConverter(event.target.value))} placeholder="5000" />
+            <InputGroup>
+                <InputLeftAddon children="$" />
+                <Input onChange={(event) => setAmount(currencyConverter(event.target.value))} placeholder="5000" />
+            </InputGroup>
             <Box bg="white" h="250px" w="400px" p={6} borderWidth="1px" borderRadius="lg">
                 <Text fontSize="4xl">{amount}</Text>
             </Box>
